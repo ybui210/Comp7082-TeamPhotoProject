86b22d57d031555187c7377aceed00ad090bb2b3
Kris Chan <bigmidget489@gmail.com>
2021-10-04 23:47:39 -0700

    added social media share

--- app/src/main/java/com/example/photogallery/MainActivity.java
+++ app/src/main/java/com/example/photogallery/MainActivity.java
@@ -177,4 +177,15 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(MainActivity.this, SearchActivity.class));
 
     }
+
+    public void shareToSocialMedia(View view) {
+        File photoFile = new File(photos.get(index));
+        Uri photo = FileProvider.getUriForFile(this, "com.example.photogallery", photoFile);
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_STREAM, photo);
+        sendIntent.setType("image/*");
+        startActivity(Intent.createChooser(sendIntent,"Send"));
+
+    }
 }
